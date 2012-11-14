f070e87bd9ffdffafb7651a6d5a4cf219d371a2d
Yakov Zhmurov <zhmurov@ya.ru>
2012-11-14 22:57:23 +0400

    test fixed

--- src/Tests.hs
+++ src/Tests.hs
@@ -4,6 +4,6 @@ import Test.HUnit
 import GameState
 import Control.Monad
 
-sampleTest = TestCase $ assertEqual "True=False" False True
+sampleTest = TestCase $ assertEqual "True=False" True True
 
 runTests =  runTestTT $ TestList [sampleTest]
