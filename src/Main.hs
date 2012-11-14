fabc11777d7b185e2a9347d613c3c50e6f3b1527
Yakov Zhmurov <zhmurov@ya.ru>
2012-11-14 23:02:24 +0400

    pause on tests fail

--- src/Main.hs
+++ src/Main.hs
@@ -7,9 +7,11 @@ import Control.Monad
 import Test.HUnit
 
 main = do
-	testResults <- runTests
-	unless (failures testResults == 0) $ error "Tests failed"
-	run $ newGame { 
+    testResults <- runTests
+    unless (failures testResults == 0) $ do getChar
+                                            error "Tests failed"
+                                            return ()
+    run $ newGame { 
               load = loadGame,
               move = moveGame,
               render = renderGame,
