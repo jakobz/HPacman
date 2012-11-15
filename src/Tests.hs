1310b4d801e92e50cb4f530852acd5dea914428a
Yakov Zhmurov <zhmurov@ya.ru>
2012-11-16 02:21:47 +0400

    another segment intersection test-case added

--- src/Tests.hs
+++ src/Tests.hs
@@ -17,9 +17,11 @@ runBoxTest (c1,c2,result) = TestCase
 boxTests = map runBoxTest
            $ concatMap (\(c1,c2,result) -> [(c1,c2,result),(c2,c1,result)]) boxCases
 
-segmentsCases = [([(0,0),(2,2),(2,0),(0,2)],Just(1,1)),
-			     ([(0,0),(0,2),(2,2),(2,0)],Nothing),
-			     ([(1,-2),(1,2),(0,0),(2,0)],Just(1,0))
+segmentsCases = [([(0,0),(4,2),(4,0),(0,2)],Just(2,1)), -- cross
+			     ([(0,0),(0,2),(2,0),(2,2)],Nothing),   -- parallel
+			     ([(1,-2),(1,2),(0,0),(2,0)],Just(1,0)), -- perpendicular, parallel to axises
+			     ([(0,0),(1,0),(3,0),(4,0)],Nothing),     -- colinear
+			     ([(0,0),(0,2),(-3,0),(-2,1)],Nothing)     -- lines intersects, but segments don't
 			    ]
 
 runSegmentTest (params,result) = TestCase 
@@ -27,6 +29,7 @@ runSegmentTest (params,result) = TestCase
     $ let [c1,c2,c3,c4] = params
       in segmentsIntersects (c1,c2) (c3,c4)
 
+-- shuffle end and start points and exchange segments in all combinations
 segmentShuffles ([c1,c2,c3,c4], result) = [
 				([c1,c2,c3,c4],result),([c2,c1,c3,c4],result),([c1,c2,c4,c3],result),([c2,c1,c4,c3],result),
 				([c3,c4,c1,c2],result),([c3,c4,c2,c1],result),([c4,c3,c1,c2],result),([c4,c3,c2,c1],result)
